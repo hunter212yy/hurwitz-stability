a6160f534151e218bb4db4d8a3b9407a6915604b
Adam Kosun <kosunadam@gmail.com>
2017-01-18 10:38:56 +0100

    v0.1 First commit, working prototype

---
+++ README.md
@@ -0,0 +1,7 @@
+#hurwitz-stability
+###This program takes a linear system's characteristic equation (its coefficients) and checks the system's stability against the [Hurwitz stability criterion] (https://en.wikipedia.org/wiki/Hurwitz_matrix).
+
+##License
+#MIT License
+(Check LICENSE.md)
+
---
+++ hurwitz-stability.py
@@ -0,0 +1,64 @@
+import numpy as np
+import sys
+
+print("Jezeli wielomian mianownika jest zapisany jako: ")
+print("a0*s^n + a1*s^(n-1) + ... + a(n-1)*s + an")
+print("Podaj stopien mianownika")
+stopien = int(input('Stopien: '))
+A = []
+for x in range (0, stopien+1):
+    A.append(int(input(str('Podaj wspolczynnik '+ 'a' + str(x) + ": "))))
+
+print(A)
+
+matrix=[]
+k = 0
+
+for _ in range(0, len(A)-1):
+    column = []
+    d = 0
+    for _ in range(0, len(A)-1):
+        if 2*d+1-k < 0:
+            column.append(0)
+        else:
+            try:
+                column.append(A[2*d+1-k])
+            except IndexError:
+                column.append(0)
+        d += 1
+    matrix.append(column)
+    k += 1
+
+print('Macierz Hurwitza: \n')
+print(np.array(matrix))
+print("Wyznacznik tej macierzy jest rowny: " +
+      str(np.linalg.det(np.array(matrix))))
+if np.linalg.det(np.array(matrix)) < 0:
+    print("Uklad jest niestabilny.")
+    sys.exit()
+if np.linalg.det(np.array(matrix)) == 0:
+    print("Uklad na granicy stabilnosci.")
+
+def minor(arr, i, j):
+    # usuwa i-ty wiersz i j-ta kolumne
+    return arr[np.array(list(range(i))+list(range(i+1, arr.shape[0])))[:,np.newaxis],
+               np.array(list(range(j))+list(range(j+1, arr.shape[1])))]
+a=0
+newmatrix=np.array(matrix)
+for _ in range(0, len(A)-2):
+    x,y = newmatrix.shape
+    newmatrix= minor(np.array(newmatrix), x-1, y-1)
+    print(newmatrix)
+    print("Wyznacznik tej macierzy jest rowny: " +
+          str(np.linalg.det(newmatrix)))
+    if np.linalg.det(newmatrix) > 0:
+        continue
+    elif np.linalg.det(newmatrix) == 0:
+        print("Uklad na granicy stabilnosci")
+        continue
+    else:
+        print("Uklad jest niestabilny")
+        sys.exit()
+print("Uklad jest stabilny.")
+
+
